5c1a82032b0bb15ccb0d47bdb623384eff2b4960
JoshuaLicense <JoshuaLicense@users.noreply.github.com>
2018-03-09 17:44:06 +0000

    Add alert component
    Allow a response to be sent to the client, can specify a style for the alert (using Bootstrap's alert-* classes).

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Nav from './components/Nav';
+
+import Alert from './components/Alert';
+
 import SQL from 'sql.js';
 
 class App extends Component {
@@ -23,6 +26,7 @@ class App extends Component {
                 <div className="d-flex flex-row">
                     <Nav />
                     <main className="d-flex flex-column p-4 pr-5" style={{flexGrow: 1, overflow: "hidden"}}>
+                        <Alert data={this.state.alert} />
                         <section className="mb-3">
                         </section>
                         <section className="mb-3">
---
+++ src/components/Alert/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import PropTypes from 'prop-types';
+
+class Alert extends React.Component {
+    render() {
+        // Check if we have an alert to display
+        if(null === this.props.data) {
+            return null;
+        }
+
+        const { type, message, } = this.props.data;
+
+        return (
+            <div className={`alert alert-${type}`} role="alert">
+                {message}
+            </div>
+        );
+    }
+}
+
+export default Alert;
