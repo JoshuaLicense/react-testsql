9078a8c136191fdb2df7a7a96687f9bc1fdccd9e
JoshuaLicense <JoshuaLicense@users.noreply.github.com>
2018-03-18 19:40:28 +0000

    Move the change handler up higher
    Allows manipulation of the database state

--- src/components/Database/Import.js
+++ src/components/Database/Import.js
@@ -5,47 +5,39 @@ import PropTypes from 'prop-types';
 import ImportIcon from '../Icons/Import';
 
 class Import extends React.Component {
-    constructor(props) {
-        super(props);
+  handleChange = (e) => {
+    const files = e.target.files;
 
-        this.state = {
-            statement: null,
-        };
+    // No file selected, return
+    if (files.length === 0) return false;
+
+    const [file,] = files;
+
+    const fileReader = new FileReader();
+    fileReader.onload = () => {
+      const typedArray = new Uint8Array(fileReader.result);
+
+      // Run the submit handler from the parent component
+      this.props.changeHandler(typedArray);
     }
 
-    handleChange = (e) => {
-        const files = e.target.files;
+    // Tell the file reader to read the selected file as an array buffer
+    fileReader.readAsArrayBuffer(file);
 
-        // No file selected, return
-        if(files.length === 0) return false;
-
-        const [file, ] = files;
-
-        const fileReader = new FileReader();
-        fileReader.onload = () => {
-            const uint8 = new Uint8Array(fileReader.result);
-        }
-
-        // Tell the file reader to read the selected file as an array buffer
-        fileReader.readAsArrayBuffer(file);
-
-        // Reset the import back to blank so in theory could re-upload the same file
-        e.target.value = '';
-
-        // Run the submit handler from the parent component
-        //this.props.submitHandler(this.state.statement);
-    }
-
-    render() {
-        return (
-            <div>
-                <input type="file" className="d-none" id="ts-import" onChange={this.handleChange} />
-                <label htmlFor="ts-import" className="btn btn-light rounded-0 border-bottom">
-                    <ImportIcon />
-                </label>
-            </div>           
-        );
-    }
+    // Reset the import back to blank so in theory could re-upload the same file
+    e.target.value = '';
   }
-  
-  export default Import;
+
+  render() {
+    return (
+      <div>
+        <input type="file" className="d-none" id="ts-import" onChange={this.handleChange} />
+        <label htmlFor="ts-import" className="btn btn-light rounded-0 border-bottom">
+          <ImportIcon />
+        </label>
+      </div>
+    );
+  }
+}
+
+export default Import;
