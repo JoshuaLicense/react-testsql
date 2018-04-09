b8e0a3b203928d9fb257281f8f3cd3121dc6e5d4
JoshuaLicense <JoshuaLicense@users.noreply.github.com>
2018-04-09 22:54:06 +0100

    Fix answer keywords test

--- client/src/components/Question/answer.test.js
+++ client/src/components/Question/answer.test.js
@@ -1,56 +1,50 @@
-import { checkAnswer } from './logic';
+import { checkAnswer, KeywordError } from "./answer";
 
-it('fails on any missing keywords', () => {
-    const mockDB = {
-        exec: jest.fn(() => {
-            return {
-                columns: [],
-                values: []
-            }
-        }),
-        prepare: jest.fn(() => {
-            return {
-                columns: [],
-                values: [],
-            }
-        })
+const mockDB = {
+  exec: jest.fn(() => {
+    return {
+      columns: [],
+      values: []
     };
-    
-    const question = {
-        set: "Easy",
-        question: "Display all the orders that were made on the dates: 01/04/2012, 29/10/2012 and 13/09/2012",
-        answer: "SELECT * FROM Orders WHERE OrderDate IN('01/04/2012', '29/10/2012', '13/09/2012')",
+  }),
+  prepare: jest.fn(() => {
+    return {
+      step: jest.fn(() => false),
+      getAsObject: jest.fn(),
+      free: jest.fn()
     };
+  }),
+};
 
-    const missingKeywordsSQL = "SELECT * FROM Orders WHERE OrderDate = '01/04/2012' OR OrderDate = '29/10/2012' OR OrderDate = '13/09/2012'";
+it("fails on any missing keywords", () => {
+  const question = {
+    set: "Easy",
+    keywords: ["COUNT", "WHERE"],
+    question:
+      "Display all the orders that were made on the dates: 01/04/2012, 29/10/2012 and 13/09/2012",
+    answer:
+      "SELECT * FROM Orders WHERE OrderDate IN('01/04/2012', '29/10/2012', '13/09/2012')"
+  };
 
-    expect(checkAnswer(mockDB, question, missingKeywordsSQL)).toThrowError(KeywordError);
+  const missingKeywordsSQL =
+    "SELECT * FROM Orders WHERE OrderDate = '01/04/2012' OR OrderDate = '29/10/2012' OR OrderDate = '13/09/2012'";
+
+  expect(() => checkAnswer(mockDB, missingKeywordsSQL, question)).toThrowError(
+    KeywordError
+  );
 });
 
-it('fails on keywords order', () => {
-    const mockDB = {
-        exec: jest.fn(() => {
-            return {
-                columns: [],
-                values: []
-            }
-        }),
-        prepare: jest.fn(() => {
-            return {
-                columns: [],
-                values: [],
-            }
-        })
-    };
+it("fails on keywords order", () => {
+  const question = {
+    set: "Easy",
+    keywords: ["COUNT", "WHERE"],
+    question:
+      "Display the number of orders that have a freight charge above the average freight charge",
+    answer:
+      "SELECT COUNT(*) FROM Orders WHERE FreightCharge >= (SELECT AVG(FrieghtCharge) FROM Orders)"
+  };
 
-    const question = {
-        set: "Easy",
-        keywords: ["COUNT", "WHERE"],
-        question: "Display the number of orders that have a freight charge above the average freight charge",
-        answer: "SELECT COUNT(*) FROM Orders WHERE FreightCharge >= (SELECT AVG(FrieghtCharge) FROM Orders)",
-    };
+  const incorrectOrderingOfKeywordsSQL = "SELECT 25 WHERE 1 = COUNT(1)";
 
-    const incorrectOrderingOfKeywordsSQL = "SELECT 25 WHERE 1 = COUNT(1)";
-
-    expect(checkAnswer(mockDB, question, incorrectOrderingOfKeywordsSQL)).toThrowError(KeywordError);
+  expect(() => checkAnswer(mockDB, incorrectOrderingOfKeywordsSQL, question)).toThrowError(KeywordError);
 });
