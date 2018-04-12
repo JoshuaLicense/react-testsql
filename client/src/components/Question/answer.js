0a13cd019921e657734a70ba016a49d5f8ee2f1b
JoshuaLicense <JoshuaLicense@users.noreply.github.com>
2018-04-12 21:19:05 +0100

    Rewrite checking of a correct answer
    Allow punctuational column text differences between the model answer and the submitted answer :fire:
    
    Recorded a 50% speed improvements :run:

--- client/src/components/Question/answer.js
+++ client/src/components/Question/answer.js
@@ -1,5 +1,9 @@
 export class IncorrectAnswer extends Error {}
 
+const normalize = (string) => {
+  return string.toLowerCase().replace(/[^\w]/g, "");
+}
+
 const checkAnswer = (db, sql, question) => {
   // 1. Check to make sure syntax includes certain keywords, if set
   if (question.keywords) {
@@ -12,119 +16,55 @@ const checkAnswer = (db, sql, question) => {
 
     for (let i = 0; i < keywords.length; i++) {
       // Search for the keyword, must come after the previous keyword
-      lastIndex = sqlUppercase.indexOf(keywords[i], lastIndex + 1);
+      lastIndex = sqlUppercase.indexOf(keywords[i].toUpperCase(), lastIndex + 1);
 
       // If the keyword was not found
       if (lastIndex === -1) {
-        //throw new IncorrectAnswer(`Looking for the incursion of the keyword: ${keywords[i]}, but not found in the correct position!`);
+        throw new IncorrectAnswer(`Looking for the incursion of the keyword: ${keywords[i]}, but not found or found in the wrong position!`);
       }
     }
   }
 
-  const modelResults = [];
-  const userResults = [];
+  const [{ columns: userColumns, values: userValues }] = db.exec(sql);
+  const [{ columns: modelColumns, values: modelValues }] = db.exec(question.answer);
 
-  const stmt = db.prepare(sql);
-  const answerStmt = db.prepare(question.answer);
-
-  while (answerStmt.step()) modelResults.push(answerStmt.getAsObject());
-  while (stmt.step()) userResults.push(stmt.getAsObject());
-
-  stmt.free();
-  answerStmt.free();
-
-  // 2. Check both objects are of equal length
-  if (!userResults.length || modelResults.length !== userResults.length) {
-    //throw new IncorrectAnswer(`Expected a total of ${modelResults.length} row(s) to be returned, instead got ${userResults.length}!`);
-  }
-
-  // Extract the column names from each object
-  const userColumns = Object.keys(userResults[0]);
-  const modelColumns = Object.keys(modelResults[0]);
-
-  // 3. Check the same amount of columns exists in both the submitted query and the model query
+  // 2. Check the same amount of columns exists in both the submitted query and the model query
   if (modelColumns.length !== userColumns.length) {
-    //throw new IncorrectAnswer(`Expected only the following column(s) to be selected: ${modelColumns.join(", ")}`);
+    throw new IncorrectAnswer(`Expected a total of ${modelColumns.length} column(s) to be returned, instead got ${userColumns.length}!`);
   }
 
-  modelColumns.forEach((column) => {
+  // 3. Check both result sets are of equal length
+  if (modelValues.length !== userValues.length) {
+    throw new IncorrectAnswer(`Expected a total of ${modelValues.length} row(s) to be returned, instead got ${userValues.length}!`);
+  }
+
+  const normalizedUserColumns = userColumns.map(column => normalize(column));
+  const normalizedModelColumns = modelColumns.map(column => normalize(column));
+
+  normalizedUserColumns.forEach((column, userColumnIndex) => {
+    const modelColumnIndex = normalizedModelColumns.indexOf(column);
+
     // 4. Does the model column appear in the user selected columns
-    if(!userColumns[column]) {
-      //throw new IncorrectAnswer(`Expected only the following column(s) to be selected: ${modelColumns.join(", ")}`)
+    if(modelColumnIndex === -1) {
+      throw new IncorrectAnswer(`Expected only the following column(s) to be selected: ${modelColumns.join(", ")}`);
     }
 
-    // Retrieve an array from the model result object for this column only
-    const modelColumnValues = modelResults.map(object => object[column]);
-
-    // Retrieve an array from the model result object for this column only
-    const userColumnValues = userResults.map(object => object[column]);
+    const userColumnValues = userValues.map(result => result[userColumnIndex]);
+    const modelColumnValues = modelValues.map(result => result[modelColumnIndex]);
 
     // 5. Ensure every value appear in both set of values
-    const leftoverValues = modelColumnValues.reduce((prev, value) => {
-      console.log(prev, value);
-      return ['a'];
-    }, [])
-    console.log(modelColumnValues, userColumnValues);
+    modelColumnValues.forEach(value => {
+      // Check if this model value exists in the users result set
+      const foundIndex = userColumnValues.indexOf(value);
 
-  });
+      if(foundIndex === -1) {
+        throw new IncorrectAnswer(`The column value ${value} was not found in the column ${modelColumns[modelColumnIndex]}`);
+      }
 
-  console.log(userColumns);
-
-  console.log(modelColumns);
-
-  console.log(userResults);
-
-  console.log(modelResults);
-
-  // loop through each column
-  for (var i = 0; i < userColumns.length; i++) {
-    // construct a variable containing the row values contained in this column
-    var answerObj = modelResults
-      .map(item => {
-        return item[
-          userColumns[i]
-            // remove any unwanted punctuation
-            .replace(/[^A-Za-z()*]+/g, "")
-            // and capitalize aggregate functions
-            .replace(/(COUNT|SUM|AVG|MIN|TOTAL|MAX|SUM)\(/gi, l =>
-              l.toUpperCase()
-            )
-        ];
-      })
-      .filter(function(x) {
-        return typeof x !== "undefined";
-      });
-    // filter out the undefined values, indication of mis-matched column names
-
-    // do the same with the user's answer
-    const userObj = userResults.map(item => item[userColumns[i]]);
-
-    // checks that the columns selected are expected
-    if (answerObj.length !== userObj.length) {
-      return (
-        "Expected only the following column(s) to be selected: " +
-        modelColumns.join(", ")
-      );
-    }
-
-    let lastItem;
-    // checks every value in the users answer is included in the model answer
-    const isPresent = answerObj.every(item => {
-      lastItem = item;
-      return userObj.indexOf(item) !== -1;
+      // So it exists so remove it from the userColumnValues
+      userColumnValues.splice(foundIndex, 1);
     });
-
-    // if one column returns false, stop checking and return feedback
-    if (!isPresent) {
-      return (
-        'The row containing "' +
-        lastItem +
-        '" in the column ' +
-        userColumns[i] +
-        " wasn't found within your result set"
-      );
-    }
-  }
+  });
 
   // if the code executed to this point, the solution is valid
   return true;
