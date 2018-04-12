b98092a43882b31f213279519ed1c0e84f38d0a7
JoshuaLicense <JoshuaLicense@users.noreply.github.com>
2018-04-12 21:19:16 +0100

    Answer tests

--- client/src/components/Question/answer.test.js
+++ client/src/components/Question/answer.test.js
@@ -1,50 +1,137 @@
-import { checkAnswer, KeywordError } from "./answer";
+import checkAnswer from "./answer";
 
 const mockDB = {
-  exec: jest.fn(() => {
-    return {
-      columns: [],
-      values: []
-    };
-  }),
-  prepare: jest.fn(() => {
-    return {
-      step: jest.fn(() => false),
-      getAsObject: jest.fn(),
-      free: jest.fn()
-    };
-  }),
+  exec: jest.fn()
 };
 
-it("fails on any missing keywords", () => {
-  const question = {
-    set: "Easy",
-    keywords: ["COUNT", "WHERE"],
-    question:
-      "Display all the orders that were made on the dates: 01/04/2012, 29/10/2012 and 13/09/2012",
-    answer:
-      "SELECT * FROM Orders WHERE OrderDate IN('01/04/2012', '29/10/2012', '13/09/2012')"
+const mockQuestion = {
+  answer: ""
+};
+
+const mockAnswer = [
+  {
+    columns: ["ID", "FirstName", "LastName", "Phone"],
+    values: [[2, "Jane", "Doe", "021312"], [3, "John", "Doe", "123233"]]
+  }
+];
+
+it("fails on missing keywords", () => {
+  const mockKeywordQuestion = {
+    ...mockQuestion,
+    keywords: ["ThE", "QuiCK", "RED"]
   };
 
-  const missingKeywordsSQL =
-    "SELECT * FROM Orders WHERE OrderDate = '01/04/2012' OR OrderDate = '29/10/2012' OR OrderDate = '13/09/2012'";
+  const input = "The quick brown fox jumps over the lazy dog";
 
-  expect(() => checkAnswer(mockDB, missingKeywordsSQL, question)).toThrowError(
-    KeywordError
+  expect(() => checkAnswer(mockDB, input, mockKeywordQuestion)).toThrowError(
+    /^Looking for the incursion of the keyword: RED, but not found or found in the wrong position!$/
   );
 });
 
-it("fails on keywords order", () => {
-  const question = {
-    set: "Easy",
-    keywords: ["COUNT", "WHERE"],
-    question:
-      "Display the number of orders that have a freight charge above the average freight charge",
-    answer:
-      "SELECT COUNT(*) FROM Orders WHERE FreightCharge >= (SELECT AVG(FrieghtCharge) FROM Orders)"
+it("fails on keyword in wrong order", () => {
+  const mockKeywordQuestion = {
+    ...mockQuestion,
+    keywords: ["ThE", "BrowN", "QuiCK"]
   };
 
-  const incorrectOrderingOfKeywordsSQL = "SELECT 25 WHERE 1 = COUNT(1)";
+  const input = "The quick brown fox jumps over the lazy dog";
 
-  expect(() => checkAnswer(mockDB, incorrectOrderingOfKeywordsSQL, question)).toThrowError(KeywordError);
+  expect(() => checkAnswer(mockDB, input, mockKeywordQuestion)).toThrowError(
+    /^Looking for the incursion of the keyword: QuiCK, but not found or found in the wrong position!$/
+  );
+});
+
+it("fails on incorrect column number", () => {
+  mockDB.exec.mockReturnValueOnce([
+    {
+      columns: ["ID", "FirstName", "LastName"],
+      values: [
+        [1, "Joe", "Bloggs"],
+        [2, "Jane", "Doe"],
+        [3, "John", "Doe"],
+        [4, "Josh", "Bloggs"]
+      ]
+    }
+  ]);
+
+  mockDB.exec.mockReturnValueOnce(mockAnswer);
+
+  expect(() => checkAnswer(mockDB, "", mockQuestion)).toThrowError(
+    /^Expected a total of 4 column\(s\) to be returned, instead got 3!$/
+  );
+});
+
+it("fails on incorrect result number", () => {
+  mockDB.exec.mockReturnValueOnce([
+    {
+      columns: ["ID", "FirstName", "LastName", "Phone"],
+      values: [
+        [1, "Joe", "Bloggs"],
+        [2, "Jane", "Doe", "021312"],
+        [3, "John", "Doe", "123233"]
+      ]
+    }
+  ]);
+
+  mockDB.exec.mockReturnValueOnce(mockAnswer);
+
+  expect(() => checkAnswer(mockDB, "", mockQuestion)).toThrowError(
+    /^Expected a total of 2 row\(s\) to be returned, instead got 3!$/
+  );
+});
+
+it("fails on wrong column selection", () => {
+  mockDB.exec.mockReturnValueOnce([
+    {
+      columns: ["ID", "FirstName", "LastName", "City"],
+      values: [[2, "Jane", "Doe", "Manchester"], [3, "John", "Doe", "Leeds"]]
+    }
+  ]);
+
+  mockDB.exec.mockReturnValueOnce(mockAnswer);
+
+  expect(() => checkAnswer(mockDB, "", mockQuestion)).toThrowError(
+    /^Expected only the following column\(s\) to be selected: ID, FirstName, LastName, Phone$/
+  );
+});
+
+it("fails on non-exact match", () => {
+  mockDB.exec.mockReturnValueOnce([
+    {
+      columns: ["ID", "FirstName", "LastName", "Phone"],
+      values: [[1, "Joe", "Bloggs"], [3, "John", "Doe", "123233"]]
+    }
+  ]);
+
+  mockDB.exec.mockReturnValueOnce(mockAnswer);
+
+  expect(() => checkAnswer(mockDB, "", mockQuestion)).toThrowError(
+    /^The column value 2 was not found in the column ID$/
+  );
+
+  mockDB.exec.mockReturnValueOnce([
+    {
+      columns: ["ID", "FirStName", "LastName", "PhoNE"],
+      values: [[2, "Jane", "Doe", "0213120"], [3, "John", "Doe", "123233"]]
+    }
+  ]);
+
+  mockDB.exec.mockReturnValueOnce(mockAnswer);
+
+  expect(() => checkAnswer(mockDB, "", mockQuestion)).toThrowError(
+    /^The column value 021312 was not found in the column Phone$/
+  );
+});
+
+it("marked as correct answer", () => {
+  mockDB.exec.mockReturnValueOnce([
+    {
+      columns: ["id", "FiRStName", "LastNAme", "PhOne"],
+      values: [[2, "Jane", "Doe", "021312"], [3, "John", "Doe", "123233"]]
+    }
+  ]);
+
+  mockDB.exec.mockReturnValueOnce(mockAnswer);
+
+  expect(checkAnswer(mockDB, "", mockQuestion)).toBe(true);
 });
