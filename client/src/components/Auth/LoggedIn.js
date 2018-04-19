3e4a3814d0a476d91d864963bd53227151e64068
JoshuaLicense <JoshuaLicense@users.noreply.github.com>
2018-04-19 22:12:05 +0100

    LoggedIn App bar component
    Closes #20

--- client/src/components/Auth/LoggedIn.js
+++ client/src/components/Auth/LoggedIn.js
@@ -6,69 +6,27 @@ import IconButton from "material-ui/IconButton";
 import MenuIcon from "material-ui-icons/Menu";
 
 import AccountCircle from "material-ui-icons/AccountCircle";
+import LogoutIcon from "material-ui-icons/PowerSettingsNew";
 import DatabaseIcon from "material-ui-icons/Storage";
 import SessionIcon from "material-ui-icons/GroupWork";
 
-class LoggedIn extends React.Component {
-  state = {
-    anchorEl: null
-  };
+import ManageSession from "../Session";
+import ManageDatabase from "../SavedDatabase";
 
+class LoggedIn extends React.Component {
   handleLogout = () => this.props.logoutHandler();
 
-  handleClick = event => {
-    console.dir(event.currentTarget);
-    this.setState({ anchorEl: event.currentTarget });
-  };
-
-  handleClose = () => {
-    this.setState({ anchorEl: null });
-  };
-
   render() {
-    const { anchorEl } = this.state;
-
     return (
       <div>
+        <ManageDatabase />
+        <ManageSession />
         <IconButton
-          id="database-actions"
           color="inherit"
-          aria-label="Delete Actions"
+          aria-label="Logout"
+          onClick={this.handleLogout}
         >
-          <DatabaseIcon />
-        </IconButton>
-        <IconButton
-          id="session-actions"
-          onClick={this.handleClick}
-          color="inherit"
-          aria-label="Session Actions"
-        >
-          <SessionIcon />
-        </IconButton>
-        <Menu
-          id="database-appbar"
-          anchorEl={anchorEl}
-          open={Boolean(anchorEl)}
-          anchorOrigin={{
-            vertical: "top",
-            horizontal: "right"
-          }}
-          transformOrigin={{
-            vertical: "top",
-            horizontal: "right"
-          }}
-          onClose={this.handleClose}
-        >
-          <MenuItem>Profilex</MenuItem>
-          <MenuItem>My account</MenuItem>
-          <MenuItem onClick={this.handleLogout}>Logout</MenuItem>
-        </Menu>
-        <IconButton
-          onClick={this.handleClick}
-          color="inherit"
-          aria-label="Account"
-        >
-          <AccountCircle />
+          <LogoutIcon />
         </IconButton>
       </div>
     );
