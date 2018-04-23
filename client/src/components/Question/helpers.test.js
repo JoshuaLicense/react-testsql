21f2b0aaf96dfdc7853af415bc2be62e5fe28009
JoshuaLicense <JoshuaLicense@users.noreply.github.com>
2018-04-23 13:40:58 +0100

    Add getForeignColumns tests

--- client/src/components/Question/helpers.test.js
+++ client/src/components/Question/helpers.test.js
@@ -181,6 +181,26 @@ describe("getColumns()", () => {
       },
     ]);
   });
+
+  it("doesn't find enough columns in the database", () => {
+    mockDB.exec.mockReturnValue([
+      {
+        columns: ["cid", "name", "type", "notnull", "dflt_value", "pk"],
+        values: [
+          [0, "EmployeeID", "INTEGER", 1, null, 1],
+          [1, "LastName", "VARCHAR(20)", 1, null, 0],
+          [2, "FirstName", "VARCHAR(20)", 0, null, 0],
+          [3, "Title", "VARCHAR(60)", 0, null, 0],
+          [4, "Address", "VARCHAR(40)", 0, null, 0],
+          [5, "HireDate", "VARCHAR(25)", 1, null, 0]
+        ]
+      }
+    ]);
+
+    expect(() => getColumns(mockDB, tables, {x : 7 })).toThrowError(
+      /^Not enough columns found in the database.$/
+    );
+  });
 });
 
 describe("getRows()", () => {
@@ -206,5 +226,69 @@ describe("getRows()", () => {
     ]);
 
     expect(getRows(mockDB, "", "", 4)).toEqual([2, 1, 4, 3]);
+  });
+});
+
+describe("getForeignColumns()", () => {
+  const tables = ["Album", "Invoice", "Customer", "Artist", "Employee", "Track", "MediaType"];
+
+  it("fails as no foreign keys exist", () => {
+    // PRAMGA foreign_key_list will return a blank array upon not finding any fk's for that table
+    mockDB.exec.mockReturnValue([]);
+
+    expect(() => getForeignColumns(mockDB, tables, 2)).toThrowError(
+      /^Not enough foreign keys were found in the database.$/
+    );
+  });
+
+  it("returns 2 foreign keys", () => {
+    mockDB.exec.mockReturnValueOnce([
+      {
+        columns: ["id", "seq", "table", "from", "to", "on_update", "on_delete", "match"],
+        values: [
+          [0, 0, "Customer", "CustomerId", "CustomerId", "NO ACTION", "NO ACTION", "NONE"],
+        ]
+      }
+    ]);
+    mockDB.exec.mockReturnValueOnce([
+      {
+        columns: ["id", "seq", "table", "from", "to", "on_update", "on_delete", "match"],
+        values: [
+          [0, 0, "Employee", "SupportRepId", "EmployeeId", "NO ACTION", "NO ACTION", "NONE"],
+        ]
+      }
+    ]);
+    mockDB.exec.mockReturnValueOnce([
+      {
+        columns: ["id", "seq", "table", "from", "to", "on_update", "on_delete", "match"],
+        values: [
+          [0, 0, "Track", "TrackId", "TrackId", "NO ACTION", "NO ACTION", "NONE"],
+        ]
+      }
+    ]);
+
+    expect(getForeignColumns(mockDB, tables, 2)).toEqual(
+      [
+        { 
+          from : { 
+            table: "Customer", 
+            column: "CustomerId" 
+          }, 
+          to : { 
+            table: "Album", 
+            column: "CustomerId"
+          }
+        },
+        { 
+          from : { 
+            table: "Employee", 
+            column: "SupportRepId" 
+          }, 
+          to : { 
+            table: "Invoice", 
+            column: "EmployeeId"
+          }
+        },
+    ]);
   });
 });
