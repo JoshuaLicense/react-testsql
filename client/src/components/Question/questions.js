761d7b64607634da402697c44a23224cd3449b71
JoshuaLicense <JoshuaLicense@users.noreply.github.com>
2018-04-26 13:28:51 +0100

    Add 3 more questions

--- client/src/components/Question/questions.js
+++ client/src/components/Question/questions.js
@@ -58,7 +58,7 @@ const _questions = [
   {
     set: "Easy",
     question: "Display the {column1}'s and {column2}'s from {table} that have a {column3} of {row}",
-    answer: "SELECT `{column1}`, `{column2}` FROM {table}",
+    answer: "SELECT `{column1}`, `{column2}` FROM {table} WHERE {column3} = '{row}'",
     func: db => {
       const tables = getTables(db);
   
@@ -72,12 +72,91 @@ const _questions = [
 
       const [row] = getRows(db, table, column3, 1);
 
+      /*
+
+      const fkTables = getTables(db);
+
+      let fk = getForeignColumns(db, fkTables, 1);
+
+      //console.log(fk);
+
+      //console.log(rows);*/
+
       return {
         table,
         column1,
         column2,
         column3,
         row,
+      };
+    }
+  },
+  {
+    set: "Easy",
+    question: "Display the rows from {table} showing the highest {column1} first.",
+    answer: "SELECT * FROM {table} ORDER BY {column1} DESC",
+    func: db => {
+      const tables = getTables(db);
+  
+      const [{ table, column: column1 }] = getColumns(
+        db,
+        tables,
+        {
+          x: 1,
+          type: "int",
+        }
+      );
+
+      return {
+        table,
+        column1,
+      };
+    }
+  },
+  {
+    set: "Easy",
+    question: "Display the rows from {table} showing the highest {column1} first, using {column2} as a secondary ascending sort",
+    answer: "SELECT * FROM {table} ORDER BY {column1} DESC",
+    func: db => {
+      const tables = getTables(db);
+  
+      const [{ table, column: column1 }, { column: column2 }] = getColumns(
+        db,
+        tables,
+        {
+          x: 2
+        }
+      );
+
+      return {
+        table,
+        column1,
+        column2,
+      };
+    }
+  },
+  {
+    set: "Easy",
+    question: "Get a list of all the {table} where the {column} is {random} null",
+    answer: "SELECT * FROM {table} WHERE {column} IS {random} NULL",
+    func: db => {
+      const random = (Math.random() >= 0.5 ? "not" : "");
+
+      const tables = getTables(db);
+  
+      const [{ table, column: column }] = getColumns(
+        db,
+        tables,
+        {
+          x: 1,
+          notnull: false,
+        }
+      );
+
+      return {
+        table,
+        column,
+        random,
       };
     }
   },
