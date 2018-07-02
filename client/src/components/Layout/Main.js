e5ce57360be04b36620ca5015247882a48cec296
JoshuaLicense <JoshuaLicense@users.noreply.github.com>
2018-07-02 12:52:20 +0100

    [File Structure] Move the Section component to the layout directory

--- client/src/components/Layout/Main.js
+++ client/src/components/Layout/Main.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import Section from "../Section";
+import Section from "./Section";
 import Question from "../Question";
 
 import OutputTable from "../Database/Output";
