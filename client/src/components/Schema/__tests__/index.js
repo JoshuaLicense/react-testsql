e5f9bfac61811cb40943c303dfecd68f5527ff4b
JoshuaLicense <JoshuaLicense@users.noreply.github.com>
2018-07-02 20:45:06 +0100

    [Schema] Add tests for the Schema component

---
+++ client/src/components/Schema/__tests__/index.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { shallow } from "enzyme";
+
+import Schema from "../index";
+
+describe("Schema component", () => {
+  let component, toggleSidebarHandlerMock;
+
+  beforeEach(() => {
+    jest.resetAllMocks();
+
+    const execMock = jest.fn();
+    const currentDatabaseMock = { exec: execMock };
+
+    execMock.mockReturnValueOnce([
+      {
+        values: [["Test table 1"], ["Test table 2"], ["Test table 3"]]
+      }
+    ]);
+
+    execMock.mockReturnValue([
+      {
+        values: [[10]]
+      }
+    ]);
+
+    toggleSidebarHandlerMock = jest.fn();
+
+    component = shallow(
+      <Schema
+        toggleSidebarHandler={toggleSidebarHandlerMock}
+        currentDatabase={currentDatabaseMock}
+      />
+    ).dive();
+  });
+
+  it("returns a loading div while the schema is loading", () => {
+    component.setState({ schema: null });
+
+    component.update();
+
+    expect(component.html()).toEqual("<div>Loading...</div>");
+  });
+
+  it("loads all the schema detail", () => {
+    expect(component.state("schema")).toEqual([
+      { name: "Test table 1", count: 10 },
+      { name: "Test table 2", count: 10 },
+      { name: "Test table 3", count: 10 }
+    ]);
+  });
+
+  it("calls the prop to toggle the sidebar", () => {
+    component.instance().handleToggleSidebar();
+
+    expect(toggleSidebarHandlerMock).toHaveBeenCalledTimes(1);
+  });
+});
