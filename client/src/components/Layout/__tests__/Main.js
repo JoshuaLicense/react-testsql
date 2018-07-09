e04e8310cd6fb42a85a1dc5605b565a250f09385
JoshuaLicense <JoshuaLicense@users.noreply.github.com>
2018-07-09 13:47:20 +0100

    [Layout] Import fix to point to correct component

--- client/src/components/Layout/__tests__/Main.js
+++ client/src/components/Layout/__tests__/Main.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import { shallow } from "enzyme";
 
-import Main from "../index";
+import Main from "../Main";
 
 describe("the Main component", () => {
   let component,
