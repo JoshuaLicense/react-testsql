3e90800baf627816b8bea6f953c062e1988581a4
JoshuaLicense <JoshuaLicense@users.noreply.github.com>
2018-07-10 14:05:33 +0100

    [Layout] Add more tests covering ComponentDidMount

--- client/src/components/Layout/__tests__/Main.js
+++ client/src/components/Layout/__tests__/Main.js
@@ -4,6 +4,17 @@ import { shallow } from "enzyme";
 
 import Main from "../Main";
 
+import { saveProgress } from "../../Group/API";
+import buildQuestions from "../../../questions/utils/buildQuestions";
+
+jest.mock("../../Group/API");
+jest.mock("../../../questions/utils/buildQuestions");
+
+buildQuestions.mockResolvedValue([{}]);
+saveProgress.mockResolvedValue(true);
+
+const flushPromises = () => new Promise(resolve => setImmediate(resolve));
+
 describe("the Main component", () => {
   let component,
     handleToggleSidebarMock,
@@ -48,4 +59,72 @@ describe("the Main component", () => {
 
     expect(component.state("feedback")).toHaveProperty("timestamp");
   });
+});
+
+it("loads the an existing set of user questions for the group they are in", () => {
+  // Firstly update the user prop to mimic a user that is currently part of a group
+  const questions = [
+    {
+      mockObjectOne: "Mock question 1"
+    },
+    {
+      mockObjectTwo: "Mock question 2"
+    }
+  ];
+
+  const user = {
+    group: {
+      questions
+    }
+  };
+
+  const component = shallow(<Main user={user} />);
+
+  expect(component.state("allQuestions")).toEqual(questions);
+  expect(component.state("activeQuestion")).toEqual(questions[0]);
+});
+
+it("loads the a new set of user questions for the group they are in and saves them to the server", async () => {
+  // Firstly update the user prop to mimic a user that is currently part of a group
+  const questions = [
+    {
+      mockObjectOne: "Mock question 1"
+    },
+    {
+      mockObjectTwo: "Mock question 2"
+    }
+  ];
+
+  const user = {
+    group: null
+  };
+
+  buildQuestions.mockResolvedValueOnce(questions);
+
+  const component = shallow(<Main user={user} />);
+
+  await flushPromises();
+
+  expect(component.state("allQuestions")).toEqual(questions);
+  expect(component.state("activeQuestion")).toEqual(questions[0]);
+});
+
+it("builds a set of questions for a user not part of a group", async () => {
+  const questions = [
+    {
+      mockObjectOne: "Mock question 1"
+    },
+    {
+      mockObjectTwo: "Mock question 2"
+    }
+  ];
+
+  buildQuestions.mockResolvedValueOnce(questions);
+
+  const component = shallow(<Main user={{}} />);
+
+  await flushPromises();
+
+  expect(component.state("allQuestions")).toEqual(questions);
+  expect(component.state("activeQuestion")).toEqual(questions[0]);
 });
