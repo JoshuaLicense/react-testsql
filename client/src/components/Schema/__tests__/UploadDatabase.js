7745947ee527821135c13f6ad903fc7b79867fdf
JoshuaLicense <JoshuaLicense@users.noreply.github.com>
2018-07-23 16:39:22 +0100

    [Upload] Add tests

--- client/src/components/Schema/__tests__/UploadDatabase.js
+++ client/src/components/Schema/__tests__/UploadDatabase.js
@@ -3,42 +3,56 @@ import { shallow } from "enzyme";
 
 import UploadDatabase from "../UploadDatabase";
 
-import UserContext from "../../Auth/Context";
 import IconButton from "@material-ui/core/IconButton";
 
-jest.mock("../../Auth/Context");
+const mockUserContext = jest.fn();
+
+jest.mock("../../Auth/Context", () => ({
+  Consumer: ({ children }) => children(mockUserContext())
+}));
+
+const flushPromises = () => new Promise(resolve => setImmediate(resolve));
 
 describe("UploadDatabase component", () => {
-  beforeEach(() => {
-    // Different mocking of the consumer object so reset the modules
-    jest.resetModules();
-  });
-
-  fit("renders he UploadDatabase component", () => {
+  it("renders he UploadDatabase component", () => {
     const uploadDatabaseHandlerMock = jest.fn();
 
-    console.log(shallow(<UploadDatabase />).debug());
+    mockUserContext.mockReturnValueOnce({ user: {} });
 
     const component = shallow(
       <UploadDatabase uploadDatabaseHandler={uploadDatabaseHandlerMock} />
     ).dive();
 
-    console.log(component.debug());
-
-    //expect(component.find("input#uploadFile").length).toEqual(1);
+    expect(component.find("input#uploadFile").length).toEqual(1);
   });
 
   it("renders he UploadDatabase component as a disabled button", () => {
-    jest.mock("../../Auth/Context", () => ({
-      Consumer: props => props.children({ user: { group: {} } })
-    }));
-
     const uploadDatabaseHandlerMock = jest.fn();
+
+    mockUserContext.mockReturnValueOnce({ user: { group: true } });
 
     const component = shallow(
       <UploadDatabase uploadDatabaseHandler={uploadDatabaseHandlerMock} />
     ).dive();
 
-    expect(component.find(IconButton).props("disabled")).toBeTruthy();
+    expect(component.find(IconButton).prop("disabled")).toBeTruthy();
+  });
+
+  it("uploads the selected file", async () => {
+    const uploadDatabaseHandlerMock = jest.fn();
+
+    mockUserContext.mockReturnValueOnce({ user: { group: true } });
+
+    const component = shallow(
+      <UploadDatabase uploadDatabaseHandler={uploadDatabaseHandlerMock} />
+    );
+
+    const file = new File(["Mock Database"], "database.txt");
+
+    component.instance().handleUpload({ target: { files: [file] } });
+
+    await flushPromises();
+
+    expect(uploadDatabaseHandlerMock).toHaveBeenCalledTimes(1);
   });
 });
