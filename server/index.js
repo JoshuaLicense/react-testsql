4315ede416f4c94d8d8a6ad37926738dea182aa3
JoshuaLicense <JoshuaLicense@users.noreply.github.com>
2018-08-13 13:17:35 +0100

    [Server] Bugfix

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
-const errorHandler = require("errorhandler");
+const errorhandler = require("errorhandler");
 
 const session = require("express-session");
 const MongoStore = require("connect-mongo")(session);
