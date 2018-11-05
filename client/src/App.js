0b056828d8f337254a8e0aa57fc708f7dc9415c4
JoshuaLicense <JoshuaLicense@users.noreply.github.com>
2018-11-05 19:04:10 +0000

    [Core] Prepare for Heroku deployment

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,6 @@ import Layout from "./components/Layout";
 
 import Loadable from "react-loadable";
 import CssBaseline from "@material-ui/core/CssBaseline";
-//import SQL from "sql.js";
 // Code splitting as sql.js is big! Load the Database provider dynamically
 const LoadableDatabaseProvider = Loadable({
   loader: () =>
