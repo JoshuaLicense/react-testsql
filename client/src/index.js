7c581d39c376c0d613a94a4888d4f65d305b8b90
Joshua License <Josh2351994@hotmail.co.uk>
2019-01-26 12:05:45 +0000

    [Core] Update react-scripts templates

--- client/src/index.js
+++ client/src/index.js
@@ -5,8 +5,11 @@ import "typeface-roboto";
 
 import App from "./App";
 
-import registerServiceWorker from "./registerServiceWorker";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById("root"));
 
-registerServiceWorker();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.register();
